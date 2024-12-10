300f69f51d57f800a8f498de8730de5af2ac03c3
Catwalk-code <egorr5302@gmail.com>
2024-12-10 18:10:44 +0300

    Update main.cpp
    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #10. Каталог товаров\n";
+    cout << "Author: Egor Orlov\n";
     return 0;
 }
