e601279e971ac29effb17aefd33a21c0550bf557
Catwalk <egorr5302@gmail.com>
2024-12-10 18:22:38 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #10. Каталог товаров\n";
     cout << "Author: Egor Orlov\n";
+    cout << "Group: 24PING1D_1\n";
     return 0;
 }
