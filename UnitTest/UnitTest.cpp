35bacd4998a85a0883d6646f62f50cdb1630d98d
Catwalk-code <egorr5302@gmail.com>
2024-12-29 20:34:15 +0300

    Добавлены модульные тесты

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -46,7 +46,51 @@ namespace unittestproject
 			Assert::AreEqual(114., process(subscriptions, 9, "Закуски"),0.01);
 			delete_subscription(subscriptions, 9);
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			GoodsSubscription* subscriptions[9];
+			subscriptions[0] = build_subscription(120.5, "Промтовары");
+			subscriptions[1] = build_subscription(110.1, "Закуски");
+			subscriptions[2] = build_subscription(123.5, "Сладости");
+			subscriptions[3] = build_subscription(100.6, "Алкоголь");
+			subscriptions[4] = build_subscription(1.1, "Закуски");
+			subscriptions[5] = build_subscription(1.6, "Сладости");
+			subscriptions[6] = build_subscription(2.8, "Закуски");
+			subscriptions[7] = build_subscription(3.9, "Промтовары");
+			subscriptions[8] = build_subscription(6.7, "Промтовары");
+			Assert::AreEqual(131.1, process(subscriptions, 9, "Промтовары"), 0.01);
+			delete_subscription(subscriptions, 9);
+		}
 
-		
+		TEST_METHOD(TestMethod3)
+		{
+			GoodsSubscription* subscriptions[9];
+			subscriptions[0] = build_subscription(120.5, "Промтовары");
+			subscriptions[1] = build_subscription(110.1, "Закуски");
+			subscriptions[2] = build_subscription(123.5, "Сладости");
+			subscriptions[3] = build_subscription(100.6, "Алкоголь");
+			subscriptions[4] = build_subscription(1.1, "Закуски");
+			subscriptions[5] = build_subscription(1.6, "Сладости");
+			subscriptions[6] = build_subscription(2.8, "Закуски");
+			subscriptions[7] = build_subscription(3.9, "Промтовары");
+			subscriptions[8] = build_subscription(6.7, "Промтовары");
+			Assert::AreEqual(125.1, process(subscriptions, 9, "Сладости"), 0.01);
+			delete_subscription(subscriptions, 9);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			GoodsSubscription* subscriptions[9];
+			subscriptions[0] = build_subscription(120.5, "Промтовары");
+			subscriptions[1] = build_subscription(110.1, "Закуски");
+			subscriptions[2] = build_subscription(123.5, "Сладости");
+			subscriptions[3] = build_subscription(100.6, "Алкоголь");
+			subscriptions[4] = build_subscription(1.1, "Закуски");
+			subscriptions[5] = build_subscription(1.6, "Сладости");
+			subscriptions[6] = build_subscription(2.8, "Закуски");
+			subscriptions[7] = build_subscription(3.9, "Промтовары");
+			subscriptions[8] = build_subscription(6.7, "Промтовары");
+			Assert::AreEqual(100.6, process(subscriptions, 9, "Алкоголь"), 0.01);
+			delete_subscription(subscriptions, 9);
+		}
 	};
 }
